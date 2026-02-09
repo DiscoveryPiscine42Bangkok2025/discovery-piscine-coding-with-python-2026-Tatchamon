7954fdd6eaa48df925cb8b74db3e1f8e5b794c42
Tatchamon <99545537+Tatchamon@users.noreply.github.com>
2026-02-09 14:58:22 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+number = input()
+
+if number == "0":
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
+
+#chmod +x ./cell02/ex00/iszero.py
+#./cell02/ex00/iszero.py
---
+++ cell02/ex02/password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+user_input = input()
+
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
+
+#chmod +x ./cell02/ex02/password.py
+#./cell02/ex02/password.py
